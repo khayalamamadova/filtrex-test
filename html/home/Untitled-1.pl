8a12c9981379f23cc3034ef10e97ace6162cd4be
Khayala Mammadova <m.xayala@outlook.com>
2024-08-22 09:32:58 +0400

    first commit

---
+++ html/home/Untitled-1.pl
@@ -0,0 +1,43 @@
+                                                                <li class="nav-item hs-has-mega-menu u-header__nav-item"
+                                                                    data-event="hover"
+                                                                    data-position="left">
+                                                                    <a id="basicMegaMenu3" class="nav-link u-header__nav-link u-header__nav-link-toggle" href="javascript:;" aria-haspopup="true" aria-expanded="false">Məişət sistemləri</a>
+
+                                                                    <!-- Nav Item - Mega Menu -->
+                                                                    <div class="hs-mega-menu vmm-tfw u-header__sub-menu" aria-labelledby="basicMegaMenu3">
+                                                                        <div class="row u-header__mega-menu-wrapper">
+                                                                            <div class="col mb-3 mb-sm-0">
+                                                                                <span class="u-header__sub-menu-title">Məişət sistemləri</span>
+                                                                                <ul class="u-header__sub-menu-nav-group mb-3">
+                                                                                    <li><a class="nav-link u-header__sub-menu-nav-link" href="home-v2.html#">Əks osmos sistemləri</a></li>
+                                                                                    <li><a class="nav-link u-header__sub-menu-nav-link border-bottom pb-3" href="home-v2.html#">Bardaq</a></li>
+                                                                                </ul>
+                                                                                <!-- <span class="u-header__sub-menu-title">Video Games</span> -->
+                                                                                <ul class="u-header__sub-menu-nav-group">
+                                                                                    <li><a class="nav-link u-header__sub-menu-nav-link u-header__sub-menu-title" href="home-v2.html#">Su yumşaldıcılar</a></li>
+                                                                                    <!-- <li><a class="nav-link u-header__sub-menu-nav-link" href="home-v2.html#">Consoles</a></li>
+                                                                                    <li><a class="nav-link u-header__sub-menu-nav-link" href="home-v2.html#">Accessories</a></li> -->
+                                                                                </ul>
+                                                                            </div>
+
+                                                                            <div class="col mb-3 mb-sm-0">
+                                                                                <span class="u-header__sub-menu-title">Magistral filtrlər</span>
+                                                                                <ul class="u-header__sub-menu-nav-group">
+                                                                                    <li><a class="nav-link u-header__sub-menu-nav-link border-bottom pb-3" href="home-v2.html#">Ehtiyat hissələri</a></li>
+                                                                                    <!-- <li><a class="nav-link u-header__sub-menu-nav-link" href="home-v2.html#">Indian Classical</a></li>
+                                                                                    <li><a class="nav-link u-header__sub-menu-nav-link" href="home-v2.html#">Musical Instruments</a></li> -->
+                                                                                </ul>
+                                                                                <span class="u-header__sub-menu-title mt-3">Sənayə sistemləri</span>
+                                                                                <ul class="u-header__sub-menu-nav-group">
+                                                                                    <li><a class="nav-link u-header__sub-menu-nav-link" href="home-v2.html#">Əks osmos sistemləri</a></li>
+                                                                                    <li><a class="nav-link u-header__sub-menu-nav-link" href="home-v2.html#">Balon tipli filtrlər</a></li>
+                                                                                    <li><a class="nav-link u-header__sub-menu-nav-link" href="home-v2.html#">Çirkab suların təmizlənməsi</a></li>
+                                                                                    <li><a class="nav-link u-header__sub-menu-nav-link" href="home-v2.html#">Kuvars Qum Filteri</a></li>
+                                                                                    <li><a class="nav-link u-header__sub-menu-nav-link" href="home-v2.html#">Aktiv Karbon Filteri</a></li>
+                                                                                </ul>
+                                                                            </div>
+
+                                                                        </div>
+                                                                    </div>
+                                                                    <!-- End Nav Item - Mega Menu -->
+                                                                </li>
